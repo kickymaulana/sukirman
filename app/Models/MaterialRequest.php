8a1c9559b48af12a10788090b4ca63550f4d43d4
kickymaulana <kickymaulana@gmail.com>
2026-07-15 11:02:19 +0700

    perbaikan model

--- app/Models/MaterialRequest.php
+++ app/Models/MaterialRequest.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class MaterialRequest extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'mr_number',
+        'user_id',
+        'type',
+        'factory',
+        'allocation',
+        'status_pembelian',
+        'status_workflow',
+    ];
     public function items()
     {
         return $this->hasMany(MaterialRequestItem::class);
--- app/Models/MaterialRequestItem.php
+++ app/Models/MaterialRequestItem.php
@@ -3,9 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class MaterialRequestItem extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'material_request_id',
+        'item_code',
+        'item_name',
+        'specification',
+        'qty',
+        'unit',
+        'item_status',
+        'monthly_usage',
+        'stock_on_hand',
+        'purpose',
+    ];
+
     public function materialRequest()
     {
         return $this->belongsTo(MaterialRequest::class);
